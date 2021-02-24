c2cbae03302a3b4fbb795977876d559bb8feef2f
robertjosephderouin <robertjosephderouin@gmail.com>
2021-02-24 13:54:05 -0700

    Add idea and connection to HTML

---
+++ src/idea.js
@@ -0,0 +1,17 @@
+class Idea {
+  constructor(){
+    this.id = new Date().getMilliseconds();
+    this.title = "";
+    this.body = "";
+    this.star = false;
+  }
+  saveToStorage(){
+    //To Be Implemented
+  }
+  deleteFromStorage(){
+    //To Be Implemented
+  }
+  updateIdea(){
+    //To be Implemented
+  }
+}
