ab1330051243e62b164430a2504c3028260e954c
robertjosephderouin <robertjosephderouin@gmail.com>
2021-02-24 15:38:47 -0700

    Add render card functionality

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@
 //Have it line up next to other cards.
 
 //*Variables Go Here*
-var saveButton = document.querySelector('#save-btn');
-var inputTitle = document.querySelector('#title-input');
-var inputBody = document.querySelector('#body-input');
+var saveButton = document.querySelector('#saveBtn');
+var inputTitle = document.querySelector('#titleInput');
+var inputBody = document.querySelector('#bodyInput');
+var savedCardGrid = document.querySelector('#savedCardGrid');
 var newCard
 
 //*Data Goes Here*
@@ -27,9 +28,30 @@ function clearBodyText(){
   inputBody.value = "";
 }
 
+function renderNewCard() {
+  savedCardGrid.innerHTML = "";
+  for (var i = 0; i < newCardArray.length; i++) {
+    savedCardGrid.innerHTML +=
+      `<article id=${newCardArray[i].id}>
+        <div class="article-top">
+          <input type="checkbox" name="star" value="star" id="star"></input>
+          <label for="star"></label>
+          <button class="x-btn"></button>
+        </div>
+        <h3>${newCardArray[i].title}</h3>
+        <p>${newCardArray[i].body}</p>
+        <div class="article-bottom">
+          <button class="comment-btn"></button>
+          <p>Comment</p>
+        </div>
+      </article>`;
+  }
+}
+
 function createNewCard(){
   newCard = new Idea(inputTitle.value, inputBody.value);
   newCardArray.push(newCard);
   clearTitleText();
   clearBodyText();
+  renderNewCard();
 }
